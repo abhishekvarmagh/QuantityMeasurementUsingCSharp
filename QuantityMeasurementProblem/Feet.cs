3b035c437d0ec1e13b35224e64880525783ca7ca
abhishekvarmagh <abhishektheverma@gmail.com>
2020-08-14 14:31:07 +0530

    [Anhishek]ADDED : TestCase And Code To Perform Equality Reference Check

--- QuantityMeasurementProblem/Feet.cs
+++ QuantityMeasurementProblem/Feet.cs
@@ -1,22 +1,50 @@
-﻿namespace QuantityMeasurementProblem
+﻿// <copyright file="Feet.cs" company="BridgeLaz Solution">
+// Copyright (c) BridgeLaz Solution. All rights reserved.
+// </copyright>
+
+namespace QuantityMeasurementProblem
 {
     using System;
 
+    /// <summary>
+    /// Feet Class.
+    /// </summary>
     public class Feet
     {
-
+        /// <summary>
+        /// Get Function To Return Feet Value.
+        /// </summary>
+        /// <param name="feet">Store Feet Value.</param>
+        /// <returns>Return Feet Value.</returns>
         public double GetFeet(double feet)
         {
             return feet;
         }
 
+        /// <summary>
+        /// Method For Equality Check.
+        /// </summary>
+        /// <param name="obj">Reference Of Object.</param>
+        /// <returns>True Or False.</returns>
         public override bool Equals(object obj)
         {
             if (obj == null)
+            {
                 return false;
+            }
+
+            if (obj == this)
+            {
+                return true;
+            }
+
             return base.Equals(obj);
         }
 
+        /// <summary>
+        /// Overiding GetHashCode Method.
+        /// </summary>
+        /// <returns></returns>
         public override int GetHashCode()
         {
             return base.GetHashCode();
---
+++ QuantityMeasurementTest/QuantityMeasurementTests.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="QuantityMeasurementTest.cs" company="BridgeLaz Solution">
+// Copyright (c) BridgeLaz Solution. All rights reserved.
+// </copyright>
+
+namespace QuantityMeasurementTest
+{
+    using NUnit.Framework;
+    using QuantityMeasurementProblem;
+
+    /// <summary>
+    /// Test Class.
+    /// </summary>
+    public class QuantityMeasurementTests
+    {
+        private Feet feetOne;
+
+        /// <summary>
+        /// functions that is performed just before each test method is called.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            this.feetOne = new Feet();
+        }
+
+        /// <summary>
+        /// Given Zero Feet and Zero Feet Should Return Equal.
+        /// </summary>
+        [Test]
+        public void GivenZeroFeetAndZeroFeet_ShouldReturnEqual()
+        {
+            double value1 = this.feetOne.GetFeet(0.0);
+            double value2 = this.feetOne.GetFeet(0.0);
+            Assert.AreEqual(value1, value2);
+        }
+
+        /// <summary>
+        /// Perform Test For Equality Null Check.
+        /// </summary>
+        [Test]
+        public void GivenNullForFeet_ShouldReturnFalse()
+        {
+            Assert.IsFalse(this.feetOne.Equals(null));
+        }
+
+        /// <summary>
+        /// Perform Test For Equality Reference Check.
+        /// </summary>
+        [Test]
+        public void GivenSameReferenceForFeet_ShouldReturnTrue()
+        {
+            Feet feetTwo = this.feetOne;
+            Assert.IsTrue(this.feetOne.Equals(feetTwo));
+        }
+    }
+}
